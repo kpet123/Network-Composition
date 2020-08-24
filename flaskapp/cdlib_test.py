67e1f29f3e8ac930ef73071557b0aed66d5eb49b
kaiser-dan <dkaiser4897@gmail.com>
2020-08-24 13:30:48 -0400

    Add dendrogram JSON file generation

--- flaskapp/cdlib_test.py
+++
@@ -1,5 +0,0 @@
-from cdlib import algorithms
-import networkx as nx
-G = nx.karate_club_graph()
-coms = algorithms.louvain(G)
-print('end')
--- flaskapp/server.py
+++ flaskapp/server.py
@@ -14,16 +14,18 @@ from cdlib import algorithms
 import networkx as nx
 import infomap
 import igraph as ig
-#import _mnet as mnet
-import mnet
+import _mnet as mnet
+#import mnet
 from networkx.readwrite import json_graph
-import infomap
+import comm_det as cd
+
 # General imports
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Other imports
 import music21
+
 
 '''
 Declare app
@@ -35,8 +37,8 @@ app = Flask(__name__)
 Set up local corpus
 '''
 localCorpus = music21.corpus.corpora.LocalCorpus()
-localCorpus.addPath('../library/')
-music21.corpus.cacheMetadata()
+localCorpus.addPath('library/')
+#music21.corpus.cacheMetadata()
 
 '''
 ******************
@@ -130,134 +132,11 @@ def make_randomwalk_json(graph, encoding_method):
     rwlst = mnet.convert_flat_js(rwlst)
     return rwlst
 
-def make_communities(g, method): 
-    '''
-    Function to run community detection
-
-    Inputs:
-        g : networkx object
-            Networkx network object representing raw music data
-        method : string
-            String identifying clustering method to be used.
-            Options are (case-sensitive):
-                1) infomap
-                2) LPM
-                3) louvain
-                4) HLC
-    Returns:
-        graph : networkx object
-            Networkx network object with added community data
-    '''
-    print("*******Inside main comm function *******")
-
-
-    if method == "infomap":
-        edge_tuples = [edge.tuple for edge in g.es]
-        im = infomap.Infomap()
-        im.add_links(edge_tuples)
-        im.run("-d -N 10")
-        modules = im.get_multilevel_modules()
-        
-        # igraph non-hierarchical version
-        #infomap_partition = g.community_infomap(edge_weights='weight')
-        
-        infomap_partition_assignment = {g.vs[i]['name'] : modules[i] 
-                        for i in range(g.vcount())}
-        
-        return infomap_partition_assignment
-    
-
-    elif method == "LPM":
-        lpm_partition = g.community_label_propagation(weights='weight')
-        lpm_partition_assignment = {g.vs[i]['name'] : [lpm_partition.membership[i]]
-                        for i in range(g.vcount())}
-        
-        return lpm_partition_assignment
-    
-
-    elif method == 'louvain':
-        louvain_partition = g.community_multilevel(weights=[e['weight'] for e in g.es], return_levels=True)
-        louvain_partition_assignment = {g.vs[i]['name'] : [level.membership[i] for level in louvain_partition]
-                        for i in range(len(g.vs))}
-        
-        return louvain_partition_assignment
-
-    elif method == 'HLC':
-        coms = algorithms.hierarchical_link_community(g)
-        
-        return coms.communities
-
-        ### TRYING HLC MODULE
-        #import hlc 
-        #os.system('python hlc -o temp_hlc_clusters.txt' )
-
-
-def helper_community_detection(graph, method):
-    '''
-    Helper function to route graph and method to correct community detection
-
-    Inputs:
-        graph : networkx object
-            Networkx network object representing raw music data
-        method : string
-            String identifying clustering method to be used
-            Options are (case-sensitive):
-                1) infomap
-                2) LPM
-                3) louvain
-                4) HLC
-    Returns:
-        graph : networkx object
-            Networkx network object with added community data
-    '''
-    print("********* Inside Helper Comm Function*****")
-  
-    # igraph methods' conversion
-    if method == 'louvain':
-        # Note louvain does not apply for directed networks, be wary of the results!
-        g = ig.Graph.TupleList(graph.edges(), directed=False)
-    else:
-        g = ig.Graph.TupleList(graph.edges(), directed=True)
-    for edge in g.es:
-        src = g.vs[edge.tuple[0]]['name']
-        tgt = g.vs[edge.tuple[1]]['name']
-        try:
-            edge['weight'] = graph.get_edge_data(src, tgt)['weight']
-        except TypeError:
-            edge['weight'] = 1
-
-    partition_data = make_communities(g, method)
-    # add partition data to graph object
-    # node-based
-    if method != 'HLC':
-        for note in graph.nodes:
-            graph.nodes[note]['comm'] = partition_data[note]
-
-
-    #link-based
-    else:
-        for link in g.es:
-            for comm in partition_data:
-                if link.tuple in comm:
-                    # igraph edge atribute, don't need for final, used in debug
-                    #g.es[link.tuple]['comm'] = partition_data.index(comm)
-
-                    
-                    # set as link attribute
-                    src = g.vs[link.tuple[0]]['name']
-                    tgt = g.vs[link.tuple[1]]['name']
-                    graph.edges[(src, tgt)]['comm'] = [partition_data.index(comm)]
-        for note in graph.nodes():
-            graph.nodes[note]['comm'] = [0] 
-    
-    
-    return graph
-
 #Input weighted community graph, add pitch labels
 def make_visualizable_graph(graph, pitchdict, cur_community, changed_edges):
 
     #add communities
-    community_graph = helper_community_detection(mnet.convert_to_weighted(\
+    community_graph = cd.helper_community_detection(mnet.convert_to_weighted(\
             graph, False), cur_community)
     #weighted_graph = mnet.convert_to_weighted(graph, False)
 
@@ -303,10 +182,10 @@ key = 'A'
 
 
 #Current graph encoding, use to recalculate graph 
-cur_graph_encoding = "basic"
+cur_graph_encoding = "grouped"
 
 #Community designator
-cur_community = "louvain" 
+cur_community = "infomap" 
 
 #Current random walk encoding, determines rhythm for random walk
 cur_walk_encoding = mnet.strto16thnote
